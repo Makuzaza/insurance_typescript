f86707c972622875366c125b762e46f12528a980
Makuzaza <maria.kuznetsova@edu.bc.fi>
2024-03-13 12:53:33 +0200

    new

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 // let current: any;
 
 const insuranceRisk = () => {
+    // declare a variable score and initializes it with the value 500
     let score: number = 500;
+    // convert it to a number using parseInt, and assigns it to the variable age. set the type of the variable to number
     const age: number = parseInt((<HTMLInputElement>document.querySelector('#age')).value);
+    // set the value entered in an input field with the ID "name" from the HTML document, setting its type to string
     const name: string = (<HTMLInputElement>document.querySelector('#name')).value; 
     const answer: HTMLElement = document.querySelector('#answers')!;
     if (!age) {
         answer.innerHTML = 'Please write your age first';
         return;
     }
-    if (age != Number(age)) {
-        answer.innerHTML = 'Please write numbers in "Your age"';
-        return;
-    }
+
     console.log(name);
 
     let score_age: number = 0; 
@@ -44,9 +44,12 @@ const insuranceRisk = () => {
     let score_current: number = score_age;
 
     let checkboxes: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[name="current"]');
+    // store the values of the checked checkboxes
     let values: string[] = [];
+    // check if it is checked using a for loop
     for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked == true) {
+            // push into value array
             values.push(checkboxes[i].value);
             score_current = score_current + (score_age*0.01);
         }
@@ -58,7 +61,9 @@ const insuranceRisk = () => {
     let score_gap: number = score_age*0.05;
     let checkboxes_goodHabits: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[name="good_habits"]');
     let valuesHabits: string[] = [];
+    // check if it is checked using a for loop
     for (let i = 0; i < checkboxes_goodHabits.length; i++) {
+        // push into value array
         if (checkboxes_goodHabits[i].checked == true) {
             valuesHabits.push(checkboxes_goodHabits[i].value);
             score_goodHabits = score_goodHabits - score_gap
@@ -84,7 +89,7 @@ const insuranceRisk = () => {
     let totalscore: number;
     totalscore = score_badHabits + (score_current - score_age);
 
-    answer.innerHTML = `${name}, You choose age: ${age} <br> Your current health: <div class="total">${values}</div>
+    answer.innerHTML = `${name} <br> You choose age: ${age} <br> Your current health: <div class="total">${values}</div>
     Your good health habits: <div class="total">${valuesHabits}</div>
     Unhealthy Habits: <div class="total">${valuesBadHabits}</div> Your riskscore: <div class="score">${totalscore}</div>`;
 }
@@ -114,9 +119,9 @@ for (let i = 0; i < badHabitsCheckboxes.length; i++) {
     checkbox.addEventListener('click', insuranceRisk);
 };
 
-const calculateButton: HTMLButtonElement | null = document.querySelector('#calculateButton');
-if (calculateButton) {
-    calculateButton.addEventListener('click', insuranceRisk);
-}
+// const calculateButton: HTMLButtonElement | null = document.querySelector('#calculateButton');
+// if (calculateButton) {
+//     calculateButton.addEventListener('click', insuranceRisk);
+// }
 
 export default insuranceRisk;
